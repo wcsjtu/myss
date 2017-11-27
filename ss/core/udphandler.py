667068e4140b2e3c38bd825789f17d6f2cc0c267
王超 <wcsjtu@163.com>
2017-11-27 19:03:46 +0800

    fix bug about udp server

--- ss/core/udphandler.py
+++ ss/core/udphandler.py
@@ -140,7 +140,8 @@ class ConnHandler(object):
         header_result = parse_header(data)
         if header_result is None:
             return
-        response = b'\x00\x00\x00' + response
+        if self._tags == self.LOC_TAG:
+            response = b'\x00\x00\x00' + response
         if self.peer_sock:
             self.peer_sock.sendto(response, self._r_addr)
         else:
