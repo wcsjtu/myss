a0d7fa871265304c7acd22b9b959f1bd2d2b38d5
王超 <wcsjtu@163.com>
2017-12-08 10:47:43 +0800

    rewrite asyncdns module: 1. non-recursive implementation of doamin parsing; 2. replace `callback` list by callback dict for efficiency

--- ss/core/asyncdns.py
+++ ss/core/asyncdns.py
@@ -28,201 +28,10 @@ from ss import utils
 from ss.lru_cache import LRUCache
 from ss.ioloop import IOLoop
 
-CACHE_SWEEP_INTERVAL = 30
 
 VALID_HOSTNAME = re.compile(br"(?!-)[A-Z\d-]{1,63}(?<!-)$", re.IGNORECASE)
 
 utils.patch_socket()
-
-# rfc1035
-# format
-# +---------------------+
-# |        Header       |
-# +---------------------+
-# |       Question      | the question for the name server
-# +---------------------+
-# |        Answer       | RRs answering the question
-# +---------------------+
-# |      Authority      | RRs pointing toward an authority
-# +---------------------+
-# |      Additional     | RRs holding additional information
-# +---------------------+
-#
-# header
-#                                 1  1  1  1  1  1
-#   0  1  2  3  4  5  6  7  8  9  0  1  2  3  4  5
-# +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
-# |                      ID                       |
-# +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
-# |QR|   Opcode  |AA|TC|RD|RA|   Z    |   RCODE   |
-# +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
-# |                    QDCOUNT                    |
-# +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
-# |                    ANCOUNT                    |
-# +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
-# |                    NSCOUNT                    |
-# +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
-# |                    ARCOUNT                    |
-# +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
-
-QTYPE_ANY = 255
-QTYPE_A = 1         # 查询类型为IP地址
-QTYPE_AAAA = 28
-QTYPE_CNAME = 5     # 查询类型为规范名称
-QTYPE_NS = 2
-QCLASS_IN = 1       # 查询类为互联网地址
-
-
-def build_address(address):
-    address = address.strip(b'.')
-    labels = address.split(b'.')
-    results = []
-    for label in labels:
-        l = len(label)
-        if l > 63:
-            return None
-        results.append(utils.chr(l))
-        results.append(label)
-    results.append(b'\0')
-    return b''.join(results)
-
-
-def build_request(address, qtype):
-    request_id = os.urandom(2)
-    header = struct.pack('!BBHHHH', 1, 0, 1, 0, 0, 0)
-    addr = build_address(address)
-    qtype_qclass = struct.pack('!HH', qtype, QCLASS_IN)
-    return request_id + header + addr + qtype_qclass
-
-
-def parse_ip(addrtype, data, length, offset):
-    if addrtype == QTYPE_A:
-        return socket.inet_ntop(socket.AF_INET, data[offset:offset + length])
-    elif addrtype == QTYPE_AAAA:
-        return socket.inet_ntop(socket.AF_INET6, data[offset:offset + length])
-    elif addrtype in [QTYPE_CNAME, QTYPE_NS]:
-        return parse_name(data, offset)[1]
-    else:
-        return data[offset:offset + length]
-
-
-def parse_name(data, offset):
-    p = offset
-    labels = []
-    l = utils.ord(data[p])
-    while l > 0:
-        if (l & (128 + 64)) == (128 + 64):
-            # pointer
-            pointer = struct.unpack('!H', data[p:p + 2])[0]
-            pointer &= 0x3FFF
-            r = parse_name(data, pointer)
-            labels.append(r[1])
-            p += 2
-            # pointer is the end
-            return p - offset, b'.'.join(labels)
-        else:
-            labels.append(data[p + 1:p + 1 + l])
-            p += 1 + l
-        l = utils.ord(data[p])
-    return p - offset + 1, b'.'.join(labels)
-
-
-# rfc1035
-# record
-#                                    1  1  1  1  1  1
-#      0  1  2  3  4  5  6  7  8  9  0  1  2  3  4  5
-#    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
-#    |                                               |
-#    /                                               /
-#    /                      NAME                     /
-#    |                                               |
-#    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
-#    |                      TYPE                     |
-#    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
-#    |                     CLASS                     |
-#    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
-#    |                      TTL                      |
-#    |                                               |
-#    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
-#    |                   RDLENGTH                    |
-#    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--|
-#    /                     RDATA                     /
-#    /                                               /
-#    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
-def parse_record(data, offset, question=False):
-    nlen, name = parse_name(data, offset)
-    if not question:
-        record_type, record_class, record_ttl, record_rdlength = struct.unpack(
-            '!HHiH', data[offset + nlen:offset + nlen + 10]
-        )
-        ip = parse_ip(record_type, data, record_rdlength, offset + nlen + 10)
-        return nlen + 10 + record_rdlength, \
-            (name, ip, record_type, record_class, record_ttl)
-    else:
-        record_type, record_class = struct.unpack(
-            '!HH', data[offset + nlen:offset + nlen + 4]
-        )
-        return nlen + 4, (name, None, record_type, record_class, None, None)
-
-
-def parse_header(data):
-    if len(data) >= 12:
-        header = struct.unpack('!HBBHHHH', data[:12])
-        res_id = header[0]
-        res_qr = header[1] & 128
-        res_tc = header[1] & 2
-        res_ra = header[2] & 128
-        res_rcode = header[2] & 15
-        # assert res_tc == 0
-        # assert res_rcode in [0, 3]
-        res_qdcount = header[3]
-        res_ancount = header[4]
-        res_nscount = header[5]
-        res_arcount = header[6]
-        return (res_id, res_qr, res_tc, res_ra, res_rcode, res_qdcount,
-                res_ancount, res_nscount, res_arcount)
-    return None
-
-
-def parse_response(data):
-    try:
-        if len(data) >= 12:     # 头部12个字节
-            header = parse_header(data)
-            if not header:
-                return None
-            res_id, res_qr, res_tc, res_ra, res_rcode, res_qdcount, \
-                res_ancount, res_nscount, res_arcount = header
-
-            qds = []
-            ans = []
-            offset = 12
-            for i in range(0, res_qdcount):
-                l, r = parse_record(data, offset, True)
-                offset += l
-                if r:
-                    qds.append(r)
-            for i in range(0, res_ancount):
-                l, r = parse_record(data, offset)
-                offset += l
-                if r:
-                    ans.append(r)
-            for i in range(0, res_nscount):
-                l, r = parse_record(data, offset)
-                offset += l
-            for i in range(0, res_arcount):
-                l, r = parse_record(data, offset)
-                offset += l
-            response = DNSResponse()
-            if qds:
-                response.hostname = qds[0][0]
-            for an in qds:
-                response.questions.append((an[1], an[2], an[3]))
-            for an in ans:
-                response.answers.append((an[1], an[2], an[3]))
-            return response
-    except Exception as e:
-        logging.error(e, exe_info=True)
-        return None
 
 
 def is_valid_hostname(hostname):
@@ -233,18 +42,115 @@ def is_valid_hostname(hostname):
     return all(VALID_HOSTNAME.match(x) for x in hostname.split(b'.'))
 
 
-class DNSResponse(object):
-    def __init__(self):
-        self.hostname = None
-        self.questions = []  # each: (addr, type, class)
-        self.answers = []  # each: (addr, type, class)
-
-    def __str__(self):
-        return '%s: %s' % (self.hostname, str(self.answers))
+class InvalidDomainName(Exception):pass
 
 
-STATUS_IPV4 = 0
-STATUS_IPV6 = 1
+class RR(object):
+    """resource record"""
+
+    __slots__ = ["domain_name", "qtype", "qcls", "ttl", "value"]
+
+    def __init__(self, dn, qt, qc, ttl, val):
+        self.domain_name = dn
+        self.qtype = qt
+        self.qcls = qc
+        self.ttl = ttl
+        self.value = val
+
+
+class Response(object):
+    DOMAIN_END = b"\x00"
+
+    def __init__(self, d):
+        self._offset = 0
+        self._data = d
+
+    def cut(self, i):
+        up = self._offset + i
+        t = self._data[self._offset:up]
+        self._offset = up
+        return t
+
+    def __getitem__(self, i):
+        return self._data[i]
+
+    def cut_domain(self):
+        """get domain from repsonse, return doamin 
+        string and its length in protocol"""
+        d = self._data
+        domain_part = []
+        up = 0
+        i = self._offset
+        while d[i] != self.DOMAIN_END:
+            length = struct.unpack("!B", d[i])[0]
+            if length >= 0xc0:
+                if i >= self._offset:
+                    self._offset += 2
+                i = struct.unpack("!H", d[i:i+2])[0] -0xc000
+                continue
+            up = i + length + 1
+            domain_part.append(d[i+1:up])
+            if up >= self._offset:
+                self._offset += (length + 1)
+            i = up
+        if up >= self._offset:
+            self._offset += 1
+        return ".".join(domain_part)
+
+    
+class DNSParser(object):
+
+    QTYPE = (QTYPE_A, QTYPE_NS, QTYPE_CNAME, QTYPE_AAAA, QTYPE_ANY) = \
+        (1, 2, 5, 28, 255)
+
+    MAX_PART_LENGTH = 63
+
+    QCLASS_IN = 1
+
+
+    def build_request(self, hostname, qtype):
+        count = struct.pack("!HHHH", 1, 0, 0, 0)
+        header = os.urandom(2) + b"\x01\x00" + count
+        parts = hostname.split(".")
+        qname = []
+        for p in parts:
+            if len(p) > self.MAX_PART_LENGTH:
+                raise InvalidDomainName(p)
+            qname += [struct.pack("!B", len(p)), p]
+        qname = ''.join(qname) + b"\x00"
+        t_c = struct.pack("!HH", qtype, self.QCLASS_IN)
+        question = qname + t_c
+        return header + question
+
+    def parse_response(self, response):
+        resp = Response(response)
+        resp.cut(6)     # ID and question number
+        answer_rrs, authority_rrs, addtional_rrs = \
+            struct.unpack("!HHH", resp.cut(6))
+        query_domain = resp.cut_domain()
+        query_type = resp.cut(2)
+        query_cls = resp.cut(2)
+
+        arrs = self.parse_rrs(resp, answer_rrs)
+        aurrs = self.parse_rrs(resp, authority_rrs)
+        adrrs = self.parse_rrs(resp, addtional_rrs)
+        rrs = arrs + aurrs + adrrs
+        return query_domain, rrs
+
+    def parse_rrs(self, resp, rrs):
+        rs = []
+        for i in xrange(rrs):
+            domain = resp.cut_domain()
+            qtype, qcls, ttl, data_length = struct.unpack("!HHIH" ,resp.cut(10))
+            if qtype == self.QTYPE_A:       # ipv4
+                data = socket.inet_ntoa(resp.cut(data_length))
+            elif qtype == self.QTYPE_AAAA:  # ipv6
+                data = socket.inet_ntop(socket.AF_INET6,resp.cut(data_length))
+            elif qtype in [self.QTYPE_NS, self.QTYPE_CNAME]:   # cname
+                data = resp.cut_domain()
+            record = RR(domain, qtype, qcls, ttl, data)
+            rs.append(record)
+        return rs
 
 
 class DNSResolver(object):
@@ -252,10 +158,10 @@ class DNSResolver(object):
     def __init__(self, io_loop, **config):
         self.io_loop = io_loop
         self._config = config
+        self._dns_parser = DNSParser()
         self._hosts = {}
+        self._cbs = {}  # {hostname: {cb:None, cb1:None}}
         self._hostname_status = {}
-        self._hostname_to_cb = {}
-        self._cb_to_hostname = {}
         self._cache = LRUCache(maxsize=10000)
         self._sock = None
         self._registered = False
@@ -322,43 +228,45 @@ class DNSResolver(object):
         self._registered = True
 
     def _call_callback(self, hostname, ip, error=None):
-        callbacks = self._hostname_to_cb.get(hostname, [])
+
+
+        callbacks = self._cbs[hostname].keys()
         for callback in callbacks:
-            if callback in self._cb_to_hostname:
-                del self._cb_to_hostname[callback]
             if ip or error:
                 callback((hostname, ip), error)
             else:
                 callback((hostname, None),
                          Exception('unknown hostname %s' % hostname))
-        if hostname in self._hostname_to_cb:
-            del self._hostname_to_cb[hostname]
-        if hostname in self._hostname_status:
-            del self._hostname_status[hostname]
+        self.del_callbacks(hostname)   # remove completed callback
+        if hostname in self._hostname_status:       
+            del self._hostname_status[hostname]     # remove qtype of hostname. 
 
     def _handle_data(self, data):
-        response = parse_response(data)
-        if response and response.hostname:
-            hostname = response.hostname
-            ip = None
-            for answer in response.answers:
-                if answer[1] in (QTYPE_A, QTYPE_AAAA) and \
-                        answer[2] == QCLASS_IN:
-                    ip = answer[0]
-                    break
-            if not ip and self._hostname_status.get(hostname, STATUS_IPV6) \
-                    == STATUS_IPV4:
-                self._hostname_status[hostname] = STATUS_IPV6
-                self._send_req(hostname, QTYPE_AAAA)
-            else:
-                if ip:
-                    self._cache[hostname] = ip
-                    self._call_callback(hostname, ip)
-                elif self._hostname_status.get(hostname, None) == STATUS_IPV6:
-                    for question in response.questions:
-                        if question[1] == QTYPE_AAAA:
-                            self._call_callback(hostname, None)
-                            break
+        try:
+            hostname, rrs = self._dns_parser.parse_response(data)
+        except Exception as e:
+            logging.warn("parse dns response error: %s" % str(e), exe_info=True)
+            return
+        ip = ""
+        for rr in rrs:
+            if rr.qtype in [DNSParser.QTYPE_A, DNSParser.QTYPE_AAAA) and \
+                rr.qcls == DNSParser.QCLASS_IN:
+                ip = rr.value
+                break
+        qtype = self._hostname_status.get(hostname, DNSParser.QTYPE_AAAA)
+        if not ip and qtype == DNSParser.QTYPE_A:   # if ipv4 didn't get an ip, try ipv6 again
+            self._send_req(hostname, QTYPE_AAAA)    
+            self._hostname_status[hostname] = DNSParser.QTYPE_AAAA  # update qtype
+        elif ip:
+            self._cache[hostname] = ip
+            self._call_callback(hostname, ip)
+        elif qtype == DNSParser.QTYPE_AAAA:
+            logging.info("unable to resolve %s using both ipv4 and ipv6" % hostname)
+            self._call_callback(hostname, None)
+        else:
+            logging.warn("impossible!!")
+        
+
 
     def handle_events(self, sock, fd, event):
         if sock != self._sock:
@@ -382,27 +290,35 @@ class DNSResolver(object):
     def handle_periodic(self):
         #self._cache.sweep()
         pass
+        
+    def add_callback(self, hostname, callback):
+        cbs = self._cbs.get(hostname, {})
+        cbs.update(callback=None)
+        self._cbs.update(hostname=cbs)
+        
 
-    def remove_callback(self, callback):
-        hostname = self._cb_to_hostname.get(callback)
-        if hostname:
-            del self._cb_to_hostname[callback]
-            arr = self._hostname_to_cb.get(hostname, None)
-            if arr:
-                arr.remove(callback)
-                if not arr:
-                    del self._hostname_to_cb[hostname]
-                    if hostname in self._hostname_status:
-                        del self._hostname_status[hostname]
+    def del_callbacks(self, hostname, callback=None):
+        """if callback not given, delete all callbacks under hostname,
+        otherwise, just delete the callback of hostname"""
+        try:
+            if callback:
+                cbs = self._cbs[hostname]
+                del cbs[callback]
+                if not cbs:
+                    del self._cbs[hostname]
+            else:
+                del self._cbs[hostname]
+        except KeyError:
+            pass
 
     def _send_req(self, hostname, qtype):
-        req = build_request(hostname, qtype)
+        req = self._dns_parser.build_request(hostname, qtype)
         for server in self._servers:
             logging.debug('resolving %s with type %d using server %s',
                           hostname, qtype, server)
             self._sock.sendto(req, (server, 53))
 
-    def resolve(self, hostname, callback):
+    def resolve(self,hostname, callback):
         if type(hostname) != bytes:
             hostname = hostname.encode('utf8')
         if not hostname:
@@ -421,16 +337,16 @@ class DNSResolver(object):
             if not is_valid_hostname(hostname):
                 callback(None, Exception('invalid hostname: %s' % hostname))
                 return
-            arr = self._hostname_to_cb.get(hostname, None)
-            if not arr:
-                self._hostname_status[hostname] = STATUS_IPV4
-                self._send_req(hostname, QTYPE_A)
-                self._hostname_to_cb[hostname] = [callback]
-                self._cb_to_hostname[callback] = hostname
-            else:
-                arr.append(callback)
-                # TODO send again only if waited too long
-                self._send_req(hostname, QTYPE_A)
+            if hostname in self._cbs:        # if hostname is under-resolving, just adds cb to         
+                self.add_callback(hostname,  # callback list, doesn't send new request any more.
+                    callback)   
+                return
+            try:
+                self._send_req(hostname, DNSParser.QTYPE_A) # ipv4 first. if failed, send ipv6 req
+                self._hostname_status[hostname] = DNSParser.QTYPE_A
+                self.add_callback(hostname, callback)
+            except InvalidDomainName as e:
+                logging.warn("invalid hostname when build dns request: %s" % hostname)
 
     def destroy(self):
         if self._sock:
